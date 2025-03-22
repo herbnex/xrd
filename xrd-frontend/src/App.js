5ba86f457cdd327b4e53f66dce829b4e18480439
Hussam Shah <hussamshah@Hussams-MacBook-Air.local>
2025-03-22 15:33:21 -0700

    Initial commit

--- xrd-frontend/src/App.js
+++ xrd-frontend/src/App.js
@@ -219,7 +219,7 @@ export default function App() {
     setErrorMessage('');
 
     try {
-      const resp = await fetch('http://127.0.0.1:5000/api/simulate',{
+      const resp = await fetch('https://xrd-backend-enuq.onrender.com',{
         method:'POST',
         headers:{ 'Content-Type':'application/json' },
         body: JSON.stringify({ structure: simulationText })
